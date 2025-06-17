9552325fcb8a16a706c26aeac28374f5a806b02d
Aripez A. Hector <hear8429@gmail.com>
2025-06-17 22:07:33 +0000

    .

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ document.getElementById("greeting").textContent = "This there this is the exampl
 
 document.getElementById("h1").textContent = " This is the text i changed ";
 
+let items = document.getElementsByClassName("redclass");
+for ( let i = 0; i < items.length; i++)
+{
+    items[i].textContent= "text change";
+
+}
+const header = document.querySelector("h1");
+console.log(header);
+
+const red = document.querySelectorAll(".redclass");
+console.log(red);
+
+document.getElementById("click").addEventListener("click" ,funciton());
+{
+    alert("CLICKED BUTTON!");
+}
